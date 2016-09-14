2fc49de2d4b25fdc0386dcb1ede6829a4f3effb5
CoXier <TmWorkCC@yahoo.com>
2016-09-14 21:36:15 +0800

    init

---
+++ app/src/main/java/com/uniquestudio/lowpolyandroid/MainActivity.java
@@ -0,0 +1,28 @@
+package com.uniquestudio.lowpolyandroid;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.uniquestudio.lowpoly.LowPoly;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.iv);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.gg, options);
+        imageView.setImageBitmap(bitmap);
+        long lastTime = System.currentTimeMillis();
+        Bitmap out = LowPoly.generate(bitmap, 120);
+        Log.d("TAG", "用时:" + (System.currentTimeMillis() - lastTime) / 1000 + "s");
+        imageView.setImageBitmap(out);
+    }
+}
---
+++ lowpoly/src/main/java/com/uniquestudio/lowpoly/LowPoly.java
@@ -0,0 +1,60 @@
+package com.uniquestudio.lowpoly;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+
+/**
+ * Created by CoXier on 2016/9/14.
+ */
+public class LowPoly {
+    static {
+        System.loadLibrary("lowpoly");
+    }
+
+    public static native int[] getTriangles(int[] pixels,int width,int height,int pointNum);
+
+
+    /**
+     *
+     * @param input
+     * @param blur
+     * @return
+     */
+    public static Bitmap generate(Bitmap input,int blur){
+        int width = input.getWidth();
+        int height = input.getHeight();
+        Bitmap newImage = Bitmap.createBitmap(input.getWidth(),input.getHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(newImage);
+        Paint paint = new Paint();
+        paint.setAntiAlias(false);
+        paint.setStyle(Paint.Style.FILL);
+        int x1, x2, x3, y1, y2, y3;
+        int pixels[] = new int[width*height];
+        for (int i=0;i<height;i++){
+            for(int j=0;j<width;j++){
+                pixels[i*width+j] = input.getPixel(j,i);
+            }
+        }
+        int[] triangles = getTriangles(pixels,width,height,blur);
+        for (int i=0;i<triangles.length;i=i+6){
+            x1 = triangles[i];
+            y1 = triangles[i+1];
+            x2 = triangles[i+2];
+            y2 =  triangles[i+3];
+            x3 =  triangles[i+4];
+            y3 = triangles[i+5];
+
+            int color = input.getPixel((x1 + x2 + x3) / 3, (y1 + y2 + y3) / 3);
+            Path path = new Path();
+            path.moveTo(x1,y1);
+            path.lineTo(x2,y2);
+            path.lineTo(x3,y3);
+            path.close();
+            paint.setColor(color);
+            canvas.drawPath(path,paint);
+        }
+        return newImage;
+    }
+}
